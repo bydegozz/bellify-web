88d3ed3a42174f379d3c6c96438a02e8fe99aa1d
degozz <bydegozz@gmail.com>
2020-06-01 17:48:20 -0500

    Updates

---
+++ precache-manifest.3b7bb7be244dd1d2d70bfdb024dca823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db156314fc06c84b685065977ec9ffa7",
+    "url": "/bellify-web/index.html"
+  },
+  {
+    "revision": "a9e5e18c951e2f92fd54",
+    "url": "/bellify-web/static/css/main.a5d4d2ed.chunk.css"
+  },
+  {
+    "revision": "8208013a2b3eab34069c",
+    "url": "/bellify-web/static/js/2.05993862.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bellify-web/static/js/2.05993862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e5e18c951e2f92fd54",
+    "url": "/bellify-web/static/js/main.bc926841.chunk.js"
+  },
+  {
+    "revision": "cda3861bc087c54450fa",
+    "url": "/bellify-web/static/js/runtime-main.0186e917.js"
+  }
+]);
